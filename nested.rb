5dcaf8fbcfe221206d0f8306cd2812e40c1313d5
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-22 01:14:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         }
      }
   
-  programmer_hash[:dennis_ritchie][:languages]
+  answer = programmer_hash[:dennis_ritchie][:languages]
+  answer.to_s 
   
 end
 
