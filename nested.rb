b91f9610428d3c5ae52b91a945877cdd15f77769
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-22 01:34:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,11 +86,8 @@ def adding_matz
      }
 
   programmer_hash.store(:yukihiro_matsumoto, :known_for)
-  #programmer_hash[:yukihiro_matsumoto] = :languages
   programmer_hash[:known_for] = "Ruby"
   programmer_hash[:languages] = ["LISP", "C"]
-  #programmer_hash[:yukihiro_matsumoto][:languages] = "C"
-  puts programmer_hash
   programmer_hash
 end
 
